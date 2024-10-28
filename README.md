5aef5fc9d9c8a35eb22c27872fb5eae4cafd22e1
Eduardo1359 <eduardosouza1629@gmail.com>
2024-10-28 20:46:11 -0300

    Adicionando documentação no código

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 ## Instalação (Linux)
  
 1. Clone o repositório
+
 ``git clone https://github.com/Eduardo1359/calculadora_python.git``
+
 ``cd calculadora_python``
+
 2. Execute o Script
 ``python3 calculadora.sh``
--- calculadora.sh
+++ calculadora.sh
@@ -1,25 +1,25 @@
 # Calculadora Básica
 
-# Menu
+# Exibe um menu de operações e solicita que o usuário escolha uma opção
 Operação = int(input("1. Adição \n 2. Subtração \n 3. Multiplicação \n 4. Divisão \n Escolha uma opção: "))
 
-# Variaveis
+# Solicita que o usuário insira dois números inteiros para realizar a operação
 num1 = int(input("Digite o primeiro valor: "))
 num2 = int(input("Digite o segundo valor: "))
 
-# Operações Básicas
-if Operação == 1:
+# Realiza a operação escolhida com base na entrada do usuário
+if Operação == 1:  # Se a operação escolhida for Adição
    resultado = num1 + num2
-   print('Resultado', resultado)
-elif Operação == 2:
+   print('Resultado', resultado)  # Exibe o resultado da adição
+elif Operação == 2:  # Se a operação escolhida for Subtração
    resultado = num1 - num2
-   print('Resultado', resultado)
-elif Operação == 3:
+   print('Resultado', resultado)  # Exibe o resultado da subtração
+elif Operação == 3:  # Se a operação escolhida for Multiplicação
    resultado = num1 * num2
-   print('Resultado', resultado)
-elif Operação == 4:
-  if num2 == 0:
-    print('Não é possível dividir por zero')
+   print('Resultado', resultado)  # Exibe o resultado da multiplicação
+elif Operação == 4:  # Se a operação escolhida for Divisão
+  if num2 == 0:  # Verifica se o divisor é zero
+    print('Não é possível dividir por zero')  # Exibe uma mensagem de erro
   else:
    resultado = num1 / num2
-   print('Resultado', resultado)
+   print('Resultado', resultado)  # Exibe o resultado da divisão
